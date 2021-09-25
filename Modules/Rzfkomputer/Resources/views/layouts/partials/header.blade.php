02aa8c1ecada89301a3f00f25dfd91b7d2c60423
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2021-09-25 11:46:45 +0700

    fix cart header

--- Modules/Rzfkomputer/Resources/views/layouts/partials/header.blade.php
+++ Modules/Rzfkomputer/Resources/views/layouts/partials/header.blade.php
@@ -39,45 +39,72 @@
               <button class="cart js-cart" type="button">
                   <span class="cart__count">{{ Cart::getTotalQuantity() }}</span><i class="rzfkomputer-cart"></i>
               </button>
-              <div class="cart-list js-cart-list">
-                  <div class="cart-list__items">
-                      <table class="cart-list__table">
-                          <tbody>
-                              @foreach (Cart::getContent() as $item)
-                                  <tr class="cart-list__row">
+              <div class="cart-list js-cart-list" dataEmpty="{{ Cart::getTotalQuantity() }}">
+                  <div class="cart-list__items js-cart-list__items">
+                      @if (Cart::getTotalQuantity() == 0)
+                          <div class="cart-list__alert-empty">
+                              <p>Saat ini, Keranjang Anda kosong</p>
+                          </div>
+                      @endif
+                      <div class="cart-list__table__wrapper">
+                          <table class="cart-list__table">
+                              <tbody class="jsTbody">
+                                  @foreach (Cart::getContent() as $item)
+                                      <tr class="cart-list__row">
+                                          <td class="cart-list__img-wrapper">
+                                              <img class="cart-list__img-el"
+                                                  src="{{ asset($item->attributes->image) }}" alt="Lenovo" />
+                                          </td>
+                                          <td class="cart-list__product">
+                                              <h6 class="cart-list__product-name">{{ $item->name }}</h6>
+                                              <p class="cart-list__product-price">{{ money($item->price, 'IDR') }}</p>
+                                              <p class="cart-list__product-count">Total : {{ $item->quantity }} pcs
+                                              </p>
+                                          </td>
+                                          <td class="cart-list__delete">
+                                              <form action="{{ route('cart_destroy', $item->id) }}" method="POST">
+                                                  @csrf
+                                                  @method('DELETE')
+                                                  <button type="submit">
+                                                      <i class="rzfkomputer-trashcan"></i>
+                                                  </button>
+                                                  {{-- <button class="cart-list__delete-btn js-cart-list-delete"
+                                                      type="submit">
+                                                      <i class="rzfkomputer-trashcan"></i>
+                                                  </button> --}}
+                                              </form>
+                                          </td>
+                                      </tr>
+                                  @endforeach
+
+                                  {{-- <tr class="cart-list__row">
                                       <td class="cart-list__img-wrapper">
-                                          <img class="cart-list__img-el" src="{{ asset($item->attributes->image) }}"
+                                          <img class="cart-list__img-el" src="assets/img/dummy/lenovo-1.png"
                                               alt="Lenovo" />
                                       </td>
                                       <td class="cart-list__product">
-                                          <h6 class="cart-list__product-name">{{ $item->name }}</h6>
-                                          <p class="cart-list__product-price">{{ money($item->price, 'IDR') }}</p>
-                                          <p class="cart-list__product-count">Total : {{ $item->quantity }} pcs</p>
+                                          <h6 class="cart-list__product-name">Asus Ryzen 5</h6>
+                                          <p class="cart-list__product-price">Rp 4.500.000</p>
+                                          <p class="cart-list__product-count">Total : 1</p>
                                       </td>
-
                                       <td class="cart-list__delete">
-                                          <form action="{{ route('cart_destroy', $item->id) }}" method="POST">
-                                              @csrf
-                                              @method('DELETE')
-                                              <button type="submit">
-                                                  <i class="rzfkomputer-trashcan"></i>
-                                              </button>
-                                              {{-- <button class="cart-list__delete-btn js-cart-list-delete" type="submit">
-                                              </button> --}}
-                                          </form>
+                                          <button class="cart-list__delete-btn js-cart-list-delete" type="submit">
+                                              <i class="rzfkomputer-trashcan"></i>
+                                          </button>
                                       </td>
-                                  </tr>
-                              @endforeach
-                          </tbody>
-                      </table>
-                      <div class="cart-list__total">
-                          <span class="cart-list__total-name">Total:</span>
-                          <h5 class="cart-list__total-price">{{ money(Cart::getTotal(), 'IDR') }}</h5>
-                      </div>
-                      <div class="cart-list__button">
-                          <div class="cart-list__button--row">
-                              <a class="btn btn--primary btn--views" href="{{ route('keranjang') }}">Lihat
-                                  Keranjang</a>
+                                  </tr> --}}
+
+                              </tbody>
+                          </table>
+                          <div class="cart-list__total">
+                              <span class="cart-list__total-name">Total:</span>
+                              <h5 class="cart-list__total-price">{{ money(Cart::getTotal(), 'IDR') }}</h5>
+                              <div class="cart-list__button">
+                                  <div class="cart-list__button--row">
+                                      <a class="btn btn--primary btn--views" href="{{ route('keranjang') }}">Lihat
+                                          Keranjang</a>
+                                  </div>
+                              </div>
                           </div>
                       </div>
                   </div>
