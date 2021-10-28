00405a7b5d022f8805e8aebeea40cab27730ee13
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2021-10-28 16:27:12 +0700

    add role

--- Modules/Product/Resources/views/index.blade.php
+++ Modules/Product/Resources/views/index.blade.php
@@ -7,6 +7,7 @@
 @stop
 
 @section('content')
+
     <div class="row">
         <div class="col-12">
             <div class="card">
@@ -30,6 +31,7 @@
                 <!-- /.card-header -->
 
                 <div class="card-body">
+
                     <table id="example1" class="table table-bordered table-striped">
                         <thead>
                             <tr>
@@ -49,55 +51,55 @@
                                 <tr class="data-row">
                                     <td class="">{{ ++$i }}</td>
                                     <td class="">{{ $item->name }}</td>
-                                                                                                        <td>
+                                    <td>
 
-                                                                 @foreach ($item->category as
-                                        $cate)
-                                        {{ $cate->name }}
-                            @endforeach
-                            </td>
-                            <td class="">{{ $item->stock }}</td>
+                                        @foreach ($item->category as $cate)
+                                            {{ $cate->name }}
+                                        @endforeach
+                                    </td>
+                                    <td class="">{{ $item->stock }}</td>
                                     <td>
                                         @if ($item->promo >= 1)
-                                        <del>{{ money($item->price, 'IDR') }}</del><br>
-                                        {{ money($item->price - ($item->price * $item->promo) / 100, 'IDR') }}
+                                            <del>{{ money($item->price, 'IDR') }}</del><br>
+                                            {{ money($item->price - ($item->price * $item->promo) / 100, 'IDR') }}
                                         @else
-                                        {{ money($item->price, 'IDR') }}
+                                            {{ money($item->price, 'IDR') }}
                                         @endif
                                     </td>
-                            <td>{{ $item->promo }} %</td>
+                                    <td>{{ $item->promo }} %</td>
                                     <td>
                                         {{-- {{ $item->status }} --}}
                                         @if ($item->status == 'true')
                                             <span class="
                                 badge badge-success">Active</span>
-                            @else
-                                <span class="badge badge-danger">Off</span>
-                                @endif
-                            </td>
-                            <td>
-                                <a href="{{ route('image.index', $item) }}" class="btn btn-xs btn-primary"
-                                    data-toggle="tooltip" title="Edit Gambar"><i class="fas fa-images"></i></a>
-                                <a href="{{ route('size.index', $item) }}" class="btn btn-xs btn-primary"
-                                    data-toggle="tooltip" title="Edit Ukuran"><i class="fas fa-pencil-ruler"></i></a>
-                                <a href="{{ route('color.index', $item) }}" class="btn btn-xs btn-primary"
-                                    data-toggle="tooltip" title="Edit Warna"><i class="fas fa-palette"></i></a>
-                            </td>
-                            <td>
-                                <form action="{{ route('product.destroy', $item) }}" method="POST">
-                                    <a class="btn btn-xs btn-warning" href="{{ route('product.edit', $item) }}"
-                                        data-toggle="tooltip" title="Edit Produk {{ $item->name }}"><i
-                                            class=" fas fa-edit"></i></a>
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-xs btn-danger" data-toggle="tooltip"
-                                        title="Hapus Produk {{ $item->name }}">
-                                        <i class="fas fa-trash-alt"
-                                            onclick="return confirm('Are you sure you want to delete this item ?');"></i>
-                                    </button>
-                                </form>
-                            </td>
-                            </tr>
+                                        @else
+                                            <span class="badge badge-danger">Off</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('image.index', $item) }}" class="btn btn-xs btn-primary"
+                                            data-toggle="tooltip" title="Edit Gambar"><i class="fas fa-images"></i></a>
+                                        <a href="{{ route('size.index', $item) }}" class="btn btn-xs btn-primary"
+                                            data-toggle="tooltip" title="Edit Ukuran"><i
+                                                class="fas fa-pencil-ruler"></i></a>
+                                        <a href="{{ route('color.index', $item) }}" class="btn btn-xs btn-primary"
+                                            data-toggle="tooltip" title="Edit Warna"><i class="fas fa-palette"></i></a>
+                                    </td>
+                                    <td>
+                                        <form action="{{ route('product.destroy', $item) }}" method="POST">
+                                            <a class="btn btn-xs btn-warning" href="{{ route('product.edit', $item) }}"
+                                                data-toggle="tooltip" title="Edit Produk {{ $item->name }}"><i
+                                                    class=" fas fa-edit"></i></a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-xs btn-danger" data-toggle="tooltip"
+                                                title="Hapus Produk {{ $item->name }}">
+                                                <i class="fas fa-trash-alt"
+                                                    onclick="return confirm('Are you sure you want to delete this item ?');"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
@@ -147,16 +149,16 @@
                     <div class="form-group">
                         <label for="inputSpeksifikasi">Spesifikasi Produk</label>
                         <textarea name="spesification" id="summernote" rows="8">
-                                    <table class="customers"><tbody><tr>
-                                        <td>Spesifikasi<br></td>
-                                        <td>Ketarangan <br></td></tr><tr><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td></tr></tbody></table>
-                                                </textarea>
+                                            <table class="customers"><tbody><tr>
+                                                <td>Spesifikasi<br></td>
+                                                <td>Ketarangan <br></td></tr><tr><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td></tr><tr><td><br></td><td><br></td></tr></tbody></table>
+                                                        </textarea>
                     </div>
                     <div class="form-group">
                         <label for="inputPhoto">Gambar Produk</label>
                         <div class="input-group col-sm-10 col-md-6 col-lg-4">
                             <div class="custom-file">
-                                {!! Form::file('image', ['class' >= 'custom-file-input', 'id' => 'exampleInputFile','required']) !!}
+                                {!! Form::file('image', ['class' >= 'custom-file-input', 'id' => 'exampleInputFile', 'required']) !!}
                             </div>
                         </div>
                     </div>
